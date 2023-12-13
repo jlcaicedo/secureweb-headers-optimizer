b58d9ced7a73c0fc85e11728ca5c93578f74ee5c
Jose Caicedo <jlcaicedo@outlook.com>
2023-12-13 16:08:15 -0500

    Refine Description of WP Security Headers Plugin for Improved Understanding
    Changes made in this commit:
    - Revised the plugin description within the wp-security-headers.php file. The updated description focuses on enhancing clarity and conciseness, ensuring that the plugin's purpose and functionality are communicated more effectively to users.
    
    Purpose of This Change:
    The update to the plugin description is aimed at providing users with a clearer and more succinct understanding of what the WP Security Headers plugin offers. By refining the language and presentation of the description, we ensure that users can easily grasp the plugin's features and benefits, enhancing their experience and understanding of the plugin.
    
    Security Considerations:
    There are no security risks associated with this change. The modification involves updating the textual content of the plugin description and does not impact the functionality or security of the plugin itself. The change is focused on improving user communication and does not affect the operational aspects of the plugin.

--- wp-security-headers.php
+++ wp-security-headers.php
@@ -2,8 +2,7 @@
 /**
  *  Plugin Name: WP Security Headers
  *  Plugin URI: http://josecaicedo.co
- *  Description: Mejora la seguridad de tu sitio de WordPress estableciendo encabezados HTTP de seguridad recomendados.
- *  Author: Jose Caicedo
+ *  Description: Improve the security of your WordPress site by setting recommended security HTTP headers. *  Author: Jose Caicedo
  *  Version: 1.0
  *  Author URI: http://josecaicedo.co
  *  Text Domain: cf7-ciuu-list
@@ -15,17 +14,14 @@
 if (!defined('ABSPATH')) exit;
 
 function wpsh_security_headers() {
-    // Remover el encabezado de la versión de WordPress
     remove_action('wp_head', 'wp_generator');
 
-    // Encabezados de seguridad recomendados
     header('X-Content-Type-Options: nosniff');
     header('X-Frame-Options: SAMEORIGIN');
     header('Content-Security-Policy: default-src \'self\';');
     header('X-XSS-Protection: 1; mode=block');
     header('Referrer-Policy: no-referrer-when-downgrade');
     header('Permissions-Policy: geolocation=(self), microphone=()');
-    // Agrega más encabezados según sea necesario
 }
 
 add_action('send_headers', 'wpsh_security_headers');
